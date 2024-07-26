d5a188ebe6feadc4dba99e4dd04ba7746a311618
andikaa666 <andikapriadi11@gmail.com>
2024-07-26 09:11:54 +0700

    update error

--- app/Http/Controllers/Api/KategoriController.php
+++ app/Http/Controllers/Api/KategoriController.php
@@ -4,7 +4,7 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
-use App\Models\Kategori;
+use App\Models\kategori;
 use Validator;
 class KategoriController extends Controller
 {
